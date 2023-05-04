6430c7a9df94a4f1b13a3287040c70a8850a2fdf
LQ <LQ@gmail.com>
2023-05-04 10:45:37 +0800

    修改文章

--- vue/crmeb1/src/components/NavMenu.vue
+++ vue/crmeb1/src/components/NavMenu.vue
@@ -1,15 +1,76 @@
 <template>
-    <div>
-        <div class="nav"></div>
+    <div class="nav_menu">
+        <div class="firstMenu">营销</div>
+        <div class="secondMenu" :class="{ menuActive: curPos == menu.id }" v-for="menu in listMenu"
+            @click="changeMenu(menu.id)">
+            {{ menu.name }}
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    data() {
+        return {
+            curPos: 0,
+            listMenu: [
+                {
+                    id: 1, name: '拼团商品'
+                },
+                {
+                    id: 2, name: '拼团列表'
+                },
+                {
+                    id: 3, name: '订单'
+                },
+            ],
+        }
+    },
+    methods: {
+        changeMenu(pos) {
+            // console.log(pos);
+            this.curPos = pos
+        },
     }
+
+}
 </script>
 
 <style lang="scss" scoped>
+.firstMenu {
+    width: 100%;
+    height: 50px;
+    padding-left: 20px;
+    color: #fff;
+    box-sizing: border-box;
+    line-height: 50px;
+    cursor: pointer;
 
+}
+
+.secondMenu {
+    width: 100%;
+    height: 50px;
+    padding-left: 60px;
+    box-sizing: border-box;
+    color: #fff;
+    line-height: 50px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #283748;
+    }
+
+}
+
+
+.menuActive {
+    background-color: #fff;
+    color: #1890ff;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #fff;
+    }
+}
 </style>
